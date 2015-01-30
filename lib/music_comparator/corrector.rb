7c7e2643d21592041ece6ff7ca20e928a28f3336
Viktor Lazarev <taurus101v@gmail.com>
2015-01-30 09:11:35 +0400

    Clear title

--- lib/music_comparator/corrector.rb
+++ lib/music_comparator/corrector.rb
@@ -11,15 +11,28 @@ module MusicComparator
         parallel_options = {progress: 'Checking files'}
         parallel_options[:in_processes] = options[:jobs].floor if options[:jobs] >= 0
 
-        pattern_to_remove = /(.+[^ ]) +(\(original mix\)|- original mix) *$/i
+        original_mix_pattern = /(.+[^ ]) +(\(original mix\)|- original mix) *$/i
+
+        dashes_pattern = /^(.*[^ ]) +- +([^()]+(?:edit|mix))$/i
 
         Parallel.each(MusicComparator::Files.mp3_files(path), parallel_options) do |filename|
           TagLib::MPEG::File.open(filename) do |mp3|
-            pattern_to_remove.match(mp3.id3v2_tag.title) do |match|
+            changed = false
+
+            original_mix_pattern.match(mp3.id3v2_tag.title) do |match|
               puts "#{filename.black.bold}\n  #{match[0].cyan.bold} > #{match[1].green.bold} #{match[2].red}"
               mp3.tag.title = match[1]
-              mp3.save
+              changed = true
             end
+
+            dashes_pattern.match(mp3.id3v2_tag.title) do |match|
+              new_title = "#{ match[1] } (#{ match[2] })"
+              puts "#{filename.black.bold}\n  #{match[1].cyan.bold} - #{match[2].red}  > #{ match[1].green } #{ "(#{ match[2] })".green.bold }"
+              mp3.tag.title = new_title
+              changed = true
+            end
+
+            mp3.save if changed
           end
         end
       end
