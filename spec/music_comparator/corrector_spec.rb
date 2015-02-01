01fbf3b0f3ce116347b7503d018ffccba1eec688
Viktor Lazarev <taurus101v@gmail.com>
2015-02-01 20:14:14 +0400

    Fix rspec examples

--- spec/music_comparator/corrector_spec.rb
+++ spec/music_comparator/corrector_spec.rb
@@ -5,20 +5,19 @@ describe MusicComparator::Corrector do
   describe '#correct_title_feat' do
 
     def common_part(options)
+      feat_artist = 'Featuring Artist'
       remix  = 'Artist 2 Remix'
       title  = 'Title'
       artist = 'Artist'
 
-      feat = "#{options[:feat_shortcut]} Featuring Artist"
-
-      title_before_corrections = options[:pattern].gsub('feat', feat).gsub('title', title).gsub('remix', remix)
+      title_before_corrections = options[:pattern].gsub('feat', "#{options[:feat_shortcut]} #{feat_artist}").gsub('title', title).gsub('remix', remix)
 
       subject.instance_variable_set :@artist, artist
       subject.instance_variable_set :@title, title_before_corrections
 
       subject.send :correct_title_feat
 
-      expect(subject.instance_variable_get(:@artist)).to eq("#{artist} #{feat}")
+      expect(subject.instance_variable_get(:@artist)).to eq("#{artist} feat. #{feat_artist}")
       expect(subject.instance_variable_get(:@title)).to eq(title)
     end
 
